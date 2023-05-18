8014395c320537955d55ae80b1e65791fd7f2c6a
Snifobry <brycardsnifo@gmail.com>
2023-05-18 02:33:39 -0500

    se agrega

---
+++ vista/layouts/footer.php
@@ -0,0 +1,37 @@
+       <!-- /contenido-->
+    
+       </div> 
+       <div class="contenedor">
+       <div class="footer-row">
+       		<div class="footer-links">
+       			<h4>Compañia</h4>
+       			<ul>
+       				<li>ESTUDIANTES ITSQMET</li>
+       			</ul>
+       		</div>
+       		<div class="footer-links">
+       			<h4>Integrantes</h4>
+       			<ul>
+       				<li>Cardenas Brayan</li>
+       				<li>Narvaez Andy</li>
+       				<li>Franco</li>
+       				<li>Arial Daniel</li>
+       				<li>Tutuaña Andy</li>
+       			</ul>
+       		</div>
+       		<div class="footer-links">
+       			<h4>Siguenos</h4>
+       			<div class="social-link">
+       				<a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+       				<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+       				<a href="https://twitter.com/?lang=es"><i class="fab fa-twitter"></i></a>
+       				<a href="https://ec.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+       				<a href="#"><i class="fab fa-whatsapp"></i></a>
+       				<h4>Todos los derechos Reservados</h4>
+       				<a href="https://www.copyright.gov/espanol/"><i class="fas fa-copyright"></i></a>
+       			</div>
+       		</div>
+       </div>
+   </div>
+</body>
+</html>
---
+++ vista/layouts/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC Básico productos</title>
+    <link rel="stylesheet" href="vista/css/app.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
+
+</head>
+<body>
+<header>  
+          <div class="content">
+            <ul>
+              
+              <img class="img" src="logo.png"/>
+                      <br>
+                      <h1></h1>
+              </ul>         
+          </div>
+        </header>
+
+
+   <div class="panel">
+
+       <h1 class="text-center">MICRO MERCADO TUTI TUTI</h1>
+       <h1 class="text-center">GESTIÓN DE PRODUCTOS</h1>
+       <h2 class="text-center">TRABAJO GRUPAL</h2> 
+       <a href="index.php"><h2>Pagina principal</h2></a>
+
+       <!-- contenido-->
